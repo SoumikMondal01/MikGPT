715515149285a1ca5f1b59183b23417f699a623e
SoumikMondal01 <soumikmondal663@gmail.com>
2023-02-27 23:32:50 +0530

    update server url

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const HandleSubmit = async (e) => {
 
   // fetch data from server -> bot's response
 
-  const response = await fetch ('http://localhost:5000', {
+  const response = await fetch ('https://mikgpt.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type' : 'application/json'
